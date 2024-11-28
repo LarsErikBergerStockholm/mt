1b6c709e08b729cb90851233168fa610ba9a485c
Lars Berger <larserikberger08@gmail.com>
2024-11-28 23:05:21 +0100

    adds router and home view

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,7 @@
 <template>
-  <div class="">
-    <input
-      type="text"
-      v-model="movieTitle"
-      @keyup.enter="search"
-      placeholder="Enter movie title"
-    />
-    <button @click="search">Search</button>
-  </div>
-  <ul class="" v-if="movies.length">
-    <li class="" v-for="movie in movies" :key="movie.imdbID">
-      {{ movie.title }}
-      {{ movie.Year }}
-      <img :src="movie.Poster" alt="Image" />
-    </li>
-  </ul>
-  <p v-if="error">{{ error }}</p>
+  <router-view />
 </template>
 
-<script setup>
-import { ref } from 'vue';
-
-const movieTitle = ref('');
-const movies = ref([]);
-const error = ref(null);
-
-const key = import.meta.env.VITE_API_KEY;
-
-const search = async () => {
-  const url = `https://www.omdbapi.com/?s=${encodeURIComponent(
-    movieTitle.value
-  )}&apikey=${key}`;
-
-  try {
-    error.value = null;
-    const response = await fetch(url);
-    const data = await response.json();
-
-    if (data.Response === 'True') {
-      movies.value = data.Search;
-    } else {
-      movies.value = [];
-      error.value = data.Error;
-    }
-  } catch (err) {
-    error.value = 'Something went wrong';
-  }
-};
-</script>
+<script setup></script>
 
 <style scoped></style>
